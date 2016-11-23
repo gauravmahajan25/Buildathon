16cefc5d54be09c8b600b428a39f32888709292f
Ashok Peravali <Ashok.Kumar@McKesson.com>
2016-11-23 11:49:15 +0530

    UI Changes

--- ui/app/scripts/controllers/cabListController.js
+++ ui/app/scripts/controllers/cabListController.js
@@ -2,11 +2,14 @@
   "use strict";
   angular.module("findACab")
     .controller("CabListController",
-                ["$scope","cabService",
+                ["$scope","cabService","cabTypeService","cabOperatorService",
                  CabListController]);
 
-  function CabListController($scope,cabService) {
+
+  function CabListController($scope,cabService,cabTypeService,cabOperatorService) {
     $scope.search = {};
+    $scope.cabTypes = cabTypeService.getTypes();
+    $scope.cabOperators = cabOperatorService.getOperators();
     console.log("Entered..............");
     $scope.searchCabs = function (search) {
       console.log("Search..............");
