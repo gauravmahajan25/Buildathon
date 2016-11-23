2aa7cd576f4f258bb6f73436a440342015d8051a
gaurav_epam <gaurav_mahajan@epam.com>
2016-11-23 23:47:52 +0530

    committing the build issue.

--- domain/src/main/java/com/chooseacab/repository/impl/JpaRepositoryImpl.java
+++ domain/src/main/java/com/chooseacab/repository/impl/JpaRepositoryImpl.java
@@ -25,90 +25,89 @@ import javax.persistence.Query;
 
 public abstract class JpaRepositoryImpl<T> implements JpaRepository<T> {
 
-    /**
-     * JPA persistent unit name configured in persistence.xml
-     */
-    public static final String DNA_JPA_PERSISTENT_UNIT_NAME = "chooseacab-domain";
+	/**
+	 * JPA persistent unit name configured in persistence.xml
+	 */
+	public static final String DNA_JPA_PERSISTENT_UNIT_NAME = "chooseacab-domain";
 
-    private static final String FIND_ALL_CLAUSE = "from {0}";
+	private static final String FIND_ALL_CLAUSE = "from {0}";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JpaRepositoryImpl.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(JpaRepositoryImpl.class);
 
-    @PersistenceContext(unitName = DNA_JPA_PERSISTENT_UNIT_NAME)
-    protected EntityManager entityManager;
+	@PersistenceContext(unitName = DNA_JPA_PERSISTENT_UNIT_NAME)
+	protected EntityManager entityManager;
 
-    @SuppressWarnings("rawtypes")
-    private static final  List EMPTY_LIST = Collections
-            .unmodifiableList(new ArrayList<T>());
+	@SuppressWarnings("rawtypes")
+	private static final List EMPTY_LIST = Collections.unmodifiableList(new ArrayList());
 
-    @Autowired(required = false)
-    private Dialect dialect = null;
+	@Autowired(required = false)
+	private Dialect dialect = null;
 
-    private final Class<T> elementType; // entity type
+	private final Class<T> elementType; // entity type
 
-    @SuppressWarnings({ "unchecked" })
-    public JpaRepositoryImpl() {
-        elementType = (Class<T>) ((ParameterizedType) getClass()
-                .getGenericSuperclass()).getActualTypeArguments()[0];
-    }
+	@SuppressWarnings({ "unchecked" })
+	public JpaRepositoryImpl() {
+		elementType = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	}
 
-    @PostConstruct
-    public void initialize() {
-    }
+	@PostConstruct
+	public void initialize() {
+	}
 
-    protected Class<T> getElementType() {
-        return elementType;
-    }
+	protected Class<T> getElementType() {
+		return elementType;
+	}
 
-    @SuppressWarnings({ "unchecked" })
-    public Collection<T> findAll() {
-        String findQueryStr = MessageFormat.format(FIND_ALL_CLAUSE,
-                new Object[] { getElementType().getName() });
+	@SuppressWarnings({ "unchecked" })
+	public Collection<T> findAll() {
+		String findQueryStr = MessageFormat.format(FIND_ALL_CLAUSE, new Object[] { getElementType().getName() });
 
-        Query findQuery = entityManager.createQuery(findQueryStr);
-        return findQuery.getResultList();        
-    }
+		Query findQuery = entityManager.createQuery(findQueryStr);
+		return findQuery.getResultList();
+	}
 
-    public T findById(Object id) {
-    	return entityManager.find(getElementType(), id);        
-    }
+	public T findById(Object id) {
+		return entityManager.find(getElementType(), id);
+	}
 
-    public T makePersist(T entity) {
-        return entityManager.merge(entity);
-    }
+	public T makePersist(T entity) {
+		return entityManager.merge(entity);
+	}
 
-    public void makeRemove(T entity) {
-        entityManager.remove(entity);
-    }
+	public void makeRemove(T entity) {
+		entityManager.remove(entity);
+	}
 
-    /**
-     * @return the entityManager
-     */
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
+	/**
+	 * @return the entityManager
+	 */
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
 
-    // @NotNull
-    public final Criteria createCriteria(final String pSalias) {
-        if (StringUtils.isBlank(pSalias)) {
-            return createCriteria();
-        } else {
-            return ((Session)getEntityManager().getDelegate()).createCriteria(elementType, p_sAlias);
-        }
-    }
+	// @NotNull
+	public final Criteria createCriteria(final String pSalias) {
+		if (StringUtils.isBlank(pSalias)) {
+			return createCriteria();
+		} else {
+			return ((Session) getEntityManager().getDelegate()).createCriteria(elementType, pSalias);
+		}
+	}
 
-    public Criteria createCriteria() {
-        return ((Session)getEntityManager().getDelegate()).createCriteria(elementType);
-    }
+	public Criteria createCriteria() {
+		return ((Session) getEntityManager().getDelegate()).createCriteria(elementType);
+	}
 
-    /**
-     * Given a SQL query string, return a Hibernate SQLQuery object that can be populated with
-     * query parameters.
-     * @param query SQL Query
-     * @return Hibernate SQLQuery
-     */
-    public SQLQuery createSQLQuery(final String query) {
-        return ((Session)getEntityManager().getDelegate()).createSQLQuery(query);
-    }
+	/**
+	 * Given a SQL query string, return a Hibernate SQLQuery object that can be
+	 * populated with query parameters.
+	 * 
+	 * @param query
+	 *            SQL Query
+	 * @return Hibernate SQLQuery
+	 */
+	public SQLQuery createSQLQuery(final String query) {
+		return ((Session) getEntityManager().getDelegate()).createSQLQuery(query);
+	}
 
 }
