a5429d1d103dff0f177e1780bae3896976fcfcfb
gaurav_epam <gaurav_mahajan@epam.com>
2016-11-24 23:56:47 +0530

    Test Coverage for Type Service Implementation

---
+++ api/src/test/java/com/chooseacab/rest/type/TypeRepresentationServiceImplTest.java
@@ -0,0 +1,59 @@
+package com.chooseacab.rest.type;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
+import com.chooseacab.model.Type;
+import com.chooseacab.service.TypeService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.when;
+
+/*
+ * A test class for Type Representation. This class unit tests the name and id for a type.
+ */
+public class TypeRepresentationServiceImplTest {
+
+	@Mock
+	private TypeService typeService;
+
+	private TypeRepresentationService typeRepresentationService;
+
+	@BeforeClass
+	public void beforeTest() {
+		MockitoAnnotations.initMocks(this);
+		typeRepresentationService = new TypeRepresentationServiceImpl();
+		ReflectionTestUtils.setField(typeRepresentationService, "typeService", typeService);
+	}
+
+	/*
+	 * A test to test the type retrieval.
+	 */
+	@Test
+	public void getTypesTest() {
+
+		final Type type = new Type();
+		type.setName("TYP1");
+
+		final List<Type> types = new ArrayList<>();
+		types.add(type);
+
+		final Collection<TypeRepresentation> actual = TypeRepresentation.toRepresentations(types);
+
+		when(typeService.getTypes()).thenReturn(types);
+		final Collection<TypeRepresentation> expected = typeRepresentationService.getTypes();
+		Assert.assertEquals(actual.size(), expected.size());
+
+	}
+
+}
