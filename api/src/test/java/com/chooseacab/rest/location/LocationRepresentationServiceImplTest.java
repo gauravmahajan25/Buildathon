1cf8ef9ab179c868ffed08131e5d9d8ab0ed8d5c
GauravMahajan <gmahajan@allianceglobalservices.com>
2016-11-25 13:27:19 +0530

    Committing initial test coverage changes for Location Resource, Representation and Representation Service.

---
+++ api/src/test/java/com/chooseacab/rest/location/LocationRepresentationServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.chooseacab.rest.location;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
+import com.chooseacab.model.Location;
+import com.chooseacab.service.LocationService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.when;
+
+/*
+ * A test class for Location Representation.
+ */
+public class LocationRepresentationServiceImplTest {
+
+	@Mock
+	private LocationService locationService;
+
+	private LocationRepresentationService locationRepresentationService;
+
+	@BeforeClass
+	public void beforeTest() {
+		MockitoAnnotations.initMocks(this);
+		locationRepresentationService = new LocationRepresentationServiceImpl();
+		ReflectionTestUtils.setField(locationRepresentationService, "locationService", locationService);
+	}
+
+	/*
+	 * A test to test the Location retrieval.
+	 */
+	@Test
+	public void getTypesTest() {
+
+		final Location location = new Location();
+		location.setName("Hyderabad");
+		location.setZipCode("500001");
+
+		final List<Location> locations = new ArrayList<>();
+		locations.add(location);
+
+		final Collection<LocationRepresentation> actual = LocationRepresentation.toRepresentations(locations);
+
+		when(locationService.getAllLocations()).thenReturn(locations);
+		final Collection<LocationRepresentation> expected = locationRepresentationService.getAllLocations();
+		Assert.assertEquals(actual.size(), expected.size());
+
+	}
+
+}
---
+++ api/src/test/java/com/chooseacab/rest/location/LocationRepresentationTest.java
@@ -0,0 +1,51 @@
+package com.chooseacab.rest.type;
+
+import com.chooseacab.model.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/*
+ * A test class for Location Representation. This class unit tests the name, zip and id for a location.
+ */
+public class LocationRepresentationTest {
+
+	/*
+	 * Method to test location name.
+	 */
+	@Test
+	  public void testLocationName(){
+	    final LocationRepresentation representation = new LocationRepresentation();
+	    representation.setName("Hyderabad");
+
+	    final String locationName = representation.getName();
+	    Assert.assertEquals("Hyderabad", locationName, "Location Name equals Test passed");
+	  }
+	
+	/*
+	 * Method to test locationId.
+	 */
+	@Test
+	  public void testLocationId(){
+	    final LocationRepresentation representation = new LocationRepresentation();
+	    representation.setId(10001L);
+	    
+	    final long locationId = representation.getId();
+	    Assert.assertEquals(10001L, locationId, "Location ID equals Test passed");
+	  }
+	
+	/*
+	 * Method to test location zip code.
+	 */
+	@Test
+	  public void testZipCode(){
+	    final LocationRepresentation representation = new LocationRepresentation();
+	    representation.setName("143001");
+
+	    final String zip = representation.getName();
+	    Assert.assertNotEquals("143001", zip, "Location ZIP equals Test passed");
+	  }
+}
